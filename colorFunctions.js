59cd1488983ace9ec1b87df7c11fb7c196d94449
EFSCprofessor <53875218+EFSCprofessor@users.noreply.github.com>
2019-10-09 10:12:57 -0400

    Add files via upload

---
+++ colorFunctions.js
@@ -0,0 +1,18 @@
+/*
+ * A set of functions to change the background color
+ */
+ 
+// Change background to red
+function setRed() {
+    document.body.style.background = "red";
+}
+ 
+// Change background to blue
+function setBlue() {
+    document.body.style.background = "blue";
+}
+ 
+// Change background to yellow
+function setYellow() {
+    document.body.style.background = "yellow";
+}
